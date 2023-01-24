e63ada5ad681f15ac4fe9299bfaf18e9063c7be5
Dmitriy <dmitriy.folomkin@gmail.com>
2023-01-24 18:03:51 +0300

    -m"Глава 2 ...kj"

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -6,6 +6,10 @@ import lombok.Data;
 public class Ingredient {
 
     private final String id;
+    private final String name;
+    private final Type type;
 
-
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE;
+    }
 }
